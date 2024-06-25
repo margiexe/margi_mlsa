f2b9a7dd45dfb094cb08b4b48bb263123cd3cb0a
margi yadav <118652991+margiexe@users.noreply.github.com>
2024-06-25 17:07:58 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Margi Yadav",
+  title: "Web Developer & Competitive Programmer",
+  email: "yadavmargi11@gmail.com",
+  gitHub: "margiexe",
+  linkedIn: "margi-yadav-012b74201",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,26 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "Codeforces 🎉",
+    description: "Click on title to redirect on my profile",
+
+    url: "https://codeforces.com/profile/margi_yadav",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Codechef 🎉",
+    description: "Click on title to redirect on my profile",
+    url: "https://www.codechef.com/users/margiyict21",
   },
   {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    title: "Research fellow at SimPPL 🎉",
+    description: "Click on title to redirect on the report published",
+    url: "https://techglobalinstitute.com/research/how-social-media-influencers-are-scaling-surrogate-political-campaigns-in-global-majority-elections/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "SWE Inter at Omniasys 🎉",
+    description : "Asp.Net developer",
   },
+  
 ];
 
 const Portfolio = () => {
